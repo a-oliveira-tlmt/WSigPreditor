4bf5608530bf3c7ab9e5905b011f8e1919f95502
a-oliveira-tlmt <32546796+a-oliveira-tlmt@users.noreply.github.com>
2019-07-07 16:31:47 -0300

    Update coletaUbN2LM.sh

--- coletaUbN2LM.sh
+++ coletaUbN2LM.sh
@@ -9,16 +9,16 @@
 
 wstalist | grep mac | cut -sd\" -f4 > /tmp/maclist.dat 
 
-for mac in ‘cat /tmp/maclist.dat‘ ; do 
-   echo -n > /tmp/‘echo $mac | sed s/[:]//g‘.txt 
+for mac in `cat /tmp/maclist.dat` ; do 
+   echo -n > /tmp/`echo $mac | sed s/[:]//g`.txt 
 done 
 
 var=1 
 while [ $var -lt 60 ] ; do 
-   for mac in ‘cat /tmp/maclist.dat‘; do 
-      wstalist | grep ’mac\|\signal’ | sed ’N;s/\n//’ | \ 
-      sed s/[,]/:/g | sed s/’ ’//g | grep $mac | \ 
-      cut -d\: -f9 >> /tmp/‘echo $mac | sed s/[:]//g‘.txt 
+   for mac in `cat /tmp/maclist.dat`; do 
+      wstalist | grep 'mac\|\signal' | sed 'N;s/\n//' | \ 
+      sed s/[,]/:/g | sed s/' '//g | grep $mac | \ 
+      cut -d\: -f9 >> /tmp/`echo $mac | sed s/[:]//g`.txt 
    done 
    sleep 60 
    var=$(( $var+1 )) 
